55c4c2eebfd1156883b4bdaf98ad03b24415d6df
fb671687 <fb671687@LN01006>
2019-01-17 11:46:21 -0600

    last class

---
+++ Jeopardy/Jeopardy/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeopardy
+{
+    class Game
+    {
+        private int gameID;
+        private string name;
+        //private List categories;
+        private TimeSpan questionTimeLimit;
+
+        private int GameID { get; set; }
+        private string Name { get; set; }
+        //private List Categories { get; set; }
+        private TimeSpan QuestionTimeLimit { get; set; }
+
+        public void createGame()
+        {
+
+        }
+
+        public void editGame()
+        {
+
+        }
+
+        public void deleteGame()
+        {
+
+        }
+
+        public void importGame()
+        {
+
+        }
+
+        public void exportGame()
+        {
+
+        }
+
+        public void playGame ()
+        {
+
+        }
+    }
+}
