41bb7a6c356e42332f35fbefaf371b6a4be1b405
RyanHarpham <rh567061@southeast.edu>
2019-01-17 12:55:35 -0600

    Play Game gameboard grid

---
+++ Jeopardy/Jeopardy/frmPlayGame.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Jeopardy
+{
+    partial class frmPlayGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlGameboard = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlGameboard
+            // 
+            this.pnlGameboard.Location = new System.Drawing.Point(13, 13);
+            this.pnlGameboard.Name = "pnlGameboard";
+            this.pnlGameboard.Size = new System.Drawing.Size(775, 425);
+            this.pnlGameboard.TabIndex = 0;
+            // 
+            // frmPlayGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlGameboard);
+            this.Name = "frmPlayGame";
+            this.Text = "frmPlayGame";
+            this.Load += new System.EventHandler(this.frmPlayGame_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlGameboard;
+    }
+}
---
+++ Jeopardy/Jeopardy/frmPlayGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jeopardy
+{
+    public partial class frmPlayGame : Form
+    {
+        public frmPlayGame()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPlayGame_Load(object sender, EventArgs e)
+        {
+            // Some default options, can change later
+            int ButtonWidth = 170;
+            int ButtonHeight = 140;
+            int Distance = 20;
+            int start_x = 10;
+            int start_y = 10;
+
+            // rows and columns, will be passed in later
+            // Just some defaults for now
+            int rows = 5;
+            int columns = 6;
+
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+                {
+                    Button tmpButton = new Button();
+                    tmpButton.Top = start_x + (x * ButtonHeight + Distance);
+                    tmpButton.Left = start_y + (y * ButtonWidth + Distance);
+                    tmpButton.Width = ButtonWidth;
+                    tmpButton.Height = ButtonHeight;
+                    tmpButton.Text = "X: " + x.ToString() + " Y: " + y.ToString();
+                    // Possible add Buttonclick event etc..
+                    //this.Controls.Add(tmpButton);
+                    pnlGameboard.Controls.Add(tmpButton);
+                }
+
+            }
+
+            pnlGameboard.AutoSize = true;
+            this.AutoSize = true;
+
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
