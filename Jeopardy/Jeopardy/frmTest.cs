d54ac0c4f94163398387eaf743c7aa20b9f85063
Tyler Sweeney <ts269649@southeast.edu>
2019-01-19 15:32:50 -0600

    Clean up the frmTest1 class.

--- Jeopardy/Jeopardy/frmTest.cs
+++ Jeopardy/Jeopardy/frmTest.cs
@@ -12,67 +12,17 @@ namespace Jeopardy
 {
     public partial class frmTest : Form
     {
-        Question aQuestion = new Question();//1, 1, "TrueFalse", null, "The Answer", "True", 1, false);
-        
+        int rows = 5;
+        int columns = 6;
+        private List<Button> ButtonList = new List<Button>();
         public frmTest()
         {
             InitializeComponent();
         }
-        int rows = 5;
-        int columns = 6;
-
-        private List<Button> ButtonList = new List<Button>();
 
         private void frmTest_Load(object sender, EventArgs e)
         {
-            aQuestion.SetID(1);
-            Question anotherQuestion = new Question(1, 2, "a", "b", "state", 3, true);
-            string id = anotherQuestion.GetState().ToString();
-            MessageBox.Show(id);
-            int formWidth = this.Width;
-            int formHeight = this.Height;
-            pnlGameBoard.Height = formHeight -70;
-            pnlGameBoard.Width = formWidth -50;
-            pnlGameBoard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-
-            // rows and columns, will be passed in later
-            // Just some defaults for now
-
-
-            // Some default options, can change later
-            int ButtonWidth = (this.Width - 220) / rows;
-            int ButtonHeight = (this.Height - 50) / columns;
-            int Distance = 20;
-            int start_x = 10;
-            int start_y = 10;
-
-
-            // For each row..
-            for (int x = 0; x < rows; x++)
-            {
-                // Create that many columns until you break out of for loop
-                // Which in turn starts the next row
-                for (int y = 0; y < columns; y++)
-                {
-                    Button tmpButton = new Button();
-                    ButtonList.Add(tmpButton);
-                    tmpButton.Top = start_x + (x * ButtonHeight + Distance);
-                    tmpButton.Left = start_y + (y * ButtonWidth + Distance);
-                    tmpButton.Width = ButtonWidth;
-                    tmpButton.Height = ButtonHeight;
-                    tmpButton.Text = "X: " + x.ToString() + " Y: " + y.ToString();
-                    tmpButton.Click += new EventHandler(button_Click);
-                    // Possible add Buttonclick event etc..
-                    //this.Controls.Add(tmpButton);
-
-                    // Add the buttons to the panel, not the form itself
-                    pnlGameBoard.Controls.Add(tmpButton);
-                }
-
-            }
-            //this.Load += new System.EventHandler(this.Resize);
-
-            // this.Width = 300;
+            DrawForm();
         }
 
         private void button_Click(object sender, EventArgs e)
@@ -83,7 +33,12 @@ namespace Jeopardy
 
         private void frmTest_Resize(object sender, System.EventArgs e)
         {
-            foreach(Button b in ButtonList)
+            DrawForm();
+        }
+
+        private void DrawForm()
+        {
+            foreach (Button b in ButtonList)
             {
                 pnlGameBoard.Controls.Remove(b);
             }
@@ -92,11 +47,6 @@ namespace Jeopardy
             int formHeight = this.Height;
             pnlGameBoard.Width = formWidth - 50;
             pnlGameBoard.Height = formHeight - 70;
-
-            //// rows and columns, will be passed in later
-            //// Just some defaults for now
-            //int rows = 5;
-            //int columns = 6;
 
             // Some default options, can change later
             int ButtonWidth = (this.Width - 220) / (rows + 1);
