04b8b2fb2fbc2b46a62f33593b6dad5a58518b32
KevinVandy <kevinvandy656@gmail.com>
2019-01-22 16:21:59 -0600

    added some blank unit test classes

---
+++ Jeopardy/Jeopardy.UnitTests/QuestionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jeopardy.UnitTests
+{
+    [TestClass]
+    public class QuestionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Jeopardy/Jeopardy.UnitTests/TeamTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jeopardy.UnitTests
+{
+    [TestClass]
+    public class TeamTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
