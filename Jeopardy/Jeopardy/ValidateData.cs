bd82ecf77b14a9910ad26c699a5899c73c9be975
KevinVandy <kevinvandy656@gmail.com>
2019-01-22 15:03:24 -0600

    worked on ValidateData.cs

--- Jeopardy/Jeopardy/ValidateData.cs
+++ Jeopardy/Jeopardy/ValidateData.cs
@@ -27,14 +27,54 @@ namespace Jeopardy
 
         }
 
+        //MARK: Validate Game properties
+        public static bool ValidateGameName(string gameName)
+        {
+            if (gameName.Length > 0 && gameName.Length < 30)
+            {
+                return true;
+            }
+            return false; //TODO throw custom exception
+        }
+
+        public static bool ValidateDefaultTimeLimit(TimeSpan defaultTimeLimit)
+        {
+            TimeSpan[] validTimeLimitsInSeconds = new TimeSpan[] { new TimeSpan(0, 0, 30), new TimeSpan(0, 1, 0), new TimeSpan(0, 2, 0), new TimeSpan(0, 3, 0) };
+            if (validTimeLimitsInSeconds.Contains(defaultTimeLimit))
+            {
+                return true;
+            }
+            return false; //TODO throw custom exception
+        }
+
+        //MARK: Validate Category properties
         public static bool ValidateCategoryTitle(string title)
         {
             if (title.Length > 0 && title.Length < 128)
             {
                 return true;
             }
-            return false;
+            return false; //TODO throw custom exception
         }
-        
+
+        public static bool ValidateCategorySubtitle(string subtitle)
+        {
+            if (subtitle.Length > 0 && subtitle.Length < 128)
+            {
+                return true;
+            }
+            return false; //TODO throw custom exception
+        }
+
+        //MARK: Validate Question properties
+
+
+        //MARK: Validate Choice properties
+
+
+        //MARK: Validate Team properties
+
+
+
     }
 }
