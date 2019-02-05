8c38452fdb2fbd47e19ce931b6f8f3c77956ec7b
RyanHarpham <rh567061@southeast.edu>
2019-02-05 17:28:43 -0600

    Simple Double Jeopardy form up and running

---
+++ Jeopardy/Jeopardy/frmDoubleJeopardy.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Jeopardy
+{
+    partial class frmDoubleJeopardy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPoints = new System.Windows.Forms.TrackBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tbPoints)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Double Jeopardy!";
+            // 
+            // tbPoints
+            // 
+            this.tbPoints.Location = new System.Drawing.Point(12, 75);
+            this.tbPoints.Maximum = 5000;
+            this.tbPoints.Minimum = 100;
+            this.tbPoints.Name = "tbPoints";
+            this.tbPoints.Size = new System.Drawing.Size(447, 45);
+            this.tbPoints.TabIndex = 1;
+            this.tbPoints.Value = 100;
+            this.tbPoints.Scroll += new System.EventHandler(this.tbPoints_Scroll);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(12, 127);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(193, 42);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(266, 127);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(193, 42);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // frmDoubleJeopardy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 181);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.tbPoints);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDoubleJeopardy";
+            this.Text = "Double Jeopardy!";
+            this.Load += new System.EventHandler(this.frmDoubleJeopardy_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbPoints)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbPoints;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
